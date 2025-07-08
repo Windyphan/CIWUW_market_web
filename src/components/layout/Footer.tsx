e954bb5241d395867dd936b6f62259ccd44e2849
Windyphan <thandongdatviettatca@gmail.com>
2025-07-08 22:21:20 +0700

    Move file to new root

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
                 <div className="xl:grid xl:grid-cols-3 xl:gap-8">
                     <div className="space-y-8 xl:col-span-1">
-                        <Link href="/">
+                        <Link href="/public">
                             <Image
                                 src="/images/ciwuw-logo-white.png"
                                 alt="CIWUW"
--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -12,7 +12,7 @@ export default function Navigation() {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex-shrink-0 flex items-center">
-                        <Link href="/">
+                        <Link href="/public">
                             <Image
                                 src="/images/ciwuw-logo.png"
                                 alt="CIWUW B2B"
